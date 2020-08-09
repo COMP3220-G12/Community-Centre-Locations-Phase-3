5551e8e062e88ebea6ec67bd92ff946685306257
LamaKha <68357513+LamaKha@users.noreply.github.com>
2020-08-08 20:35:03 -0400

    Added comments re GRASP Patterns
    Resolved by adding comments to elaborate on class methods, centralized purpose and GRASP pattern. https://github.com/COMP3220-G12/Community-Centre-Locations-Phase-3/issues/9

--- src/Visualizer.java
+++ src/Visualizer.java
@@ -1,3 +1,9 @@
+/** Visualizer an abstract class that enables us to have options to view the data
+**  For now, we will just use the text terminal view only, but we can easily plug-in any other viewing mood in the future.
+**  Using polymorphism this class helps in creating pluggable software component and handle alternatives based on types.
+**  It also supports high cohesion, as all of it's elements are supporting the same purpose.
+*/
+
 import java.util.ArrayList;
 
 abstract class Visualizer implements Template
