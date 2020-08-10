fac71b1639f3e7770b46db8b8429ec750ea63a9a
Saffa Alvi <57377830+saffaalvi@users.noreply.github.com>
2020-08-09 20:08:21 -0400

    Update Template.java

--- src/Template.java
+++ src/Template.java
@@ -12,5 +12,5 @@ interface Template
     public static final String[] TYPES = {"Integer", "String", "String", "Double", "Double"};
     public static final String FILENAME = "Community_Centres.csv";
     public static final String DELIMITER = ",";
-    public static final int PRIMARY_KEY_FIELD_INDEX = 0;
+    public static final int PRIMARY_KEY_FIELD_INDEX = 2;
 }
